11642f63e38301747402742d6a99e99d84dcb133
shrijan-adhikari <adhikari.shrijan03@gmail.com>
2026-02-02 20:09:00 +0530

    code done

--- day2.c
+++ day2.c
@@ -1,3 +1,5 @@
+/*problem statement- Write a C program to delete the element at a given 1-based position pos 
+from an array of n integers.Shift remaining elements to the left. my code*/
 #include <stdio.h>
 #include <stdlib.h>
 int main()
